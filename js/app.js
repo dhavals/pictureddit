a9e14cfa14f4f16cd3cb73dfc8582721a0470c14
Dhaval Shah <dhavals@andrew.cmu.edu>
2012-09-29 04:13:36 -0400

    some dumb commit :(

---
+++ js/app.js
@@ -0,0 +1,49 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Dhaval
+ * Date: 9/28/12
+ * Time: 8:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+$(document).ready(function () {
+    $("#loadb").click(doOnClick);
+
+    var id = '';
+
+    function doOnClick()
+    {
+
+        var formed_url = '';
+        formed_url = "http://www.reddit.com/r/pics/.json?limit=3&after=" + id + "&jsonp=?&callback=?";
+        ajaxCall(formed_url);
+    }
+
+    function ajaxCall(formed_url) {
+
+        $.ajax({
+            type:"GET",
+            url: formed_url,
+            dataType:"json",
+            success: function(data) {
+                picsCallback(data);
+            },
+            cache:false
+        });
+    }
+
+    function picsCallback(data) {
+        $.each(data.data.children, function(i,item){
+            $("<img/>").attr("src", item.data.url).appendTo("#images");
+           // id = item.data.name;
+        });
+
+        id = data.data.children[data.data.children.length-1].data.name;
+    }
+
+});
+
+
+
+
