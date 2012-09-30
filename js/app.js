cfdae9d9a90c827b33b78c8840dc23a746061aa4
Dhaval Shah <dhavals@andrew.cmu.edu>
2012-09-29 20:23:59 -0400

    backward and forward works

--- js/app.js
+++ js/app.js
@@ -7,79 +7,126 @@
  */
 
 var DEFAULT_URL = 'assets/images/productNotAvailable.jpg';
+var DEFAULT_LIMIT = 10;
 
+function ImageStore(index) {
+    this.prevArray = [];
+    this.currentArray = [];
+    this.nextArray = [];
+
+    this.imageIndex = index; // always refers to the currentArray.
+}
 
 $(document).ready(function () {
 
-    $("#loadb").click(doOnClick);
 
-    var id = '';
-    var imageArray = [];
-    var imageIndex = 0;
-    var formed_url = '';
+    var imageStore = new ImageStore(0);
+    var imageBuffer = [];
+    var firstId = null;
+
+    var needsInit = true;
 
     var subreddit = 'pics';
+    var formed_url = createUrl(subreddit, "", "", DEFAULT_LIMIT);
 
-    formed_url = "http://www.reddit.com/r/" + subreddit + "/.json?limit=15&after=" + id + "&jsonp=?&callback=?";
-    ajaxCall(formed_url);
+    ajaxCall(formed_url, true);
 
+    $("#prevbutton").click(doOnPrevClick);
+    $("#nextbutton").click(doOnNextClick);
 
-    function doOnClick()
-    {
-        imageIndex++;
-        console.dir(imageArray[imageIndex].data.url);
-        $("#currentImage").attr("src", imageArray[imageIndex].data.url);
+    function createUrl(subreddit, idBefore, idAfter, limit) {
+        return "http://www.reddit.com/r/" + subreddit + "/.json?limit=" + limit
+            + "&before=" + idBefore + "&after=" + idAfter + "&jsonp=?&callback=?";
     }
 
-    function ajaxCall(formed_url) {
+    function doOnPrevClick() {
+        if (firstId === imageStore.currentArray[0].data.name && imageStore.imageIndex === 0)
+            return;
 
-        var xhr = $.ajax({
+        imageStore.imageIndex--;
+        if (imageStore.imageIndex < 0) {
+            imageStore.nextArray = imageStore.currentArray.slice(0);
+            imageStore.currentArray = imageStore.prevArray.slice(0);
+            imageStore.imageIndex = imageStore.currentArray.length - 1; // 14
+            var idBefore = imageStore.currentArray[0].data.name;
+            ajaxCall(createUrl(subreddit, idBefore, "", DEFAULT_LIMIT), false);
+        }
+        $("#currentImage").attr("src", imageStore.currentArray[imageStore.imageIndex].data.url);
+        console.log(imageStore.currentArray[imageStore.imageIndex]);
+    }
+
+    function doOnNextClick() {
+
+        imageStore.imageIndex++;
+
+        if (imageStore.imageIndex === imageStore.currentArray.length) {
+            imageStore.prevArray = imageStore.currentArray.slice(0);
+            imageStore.currentArray = imageStore.nextArray.slice(0);
+            imageStore.imageIndex = 0;
+            var idAfter = imageStore.currentArray[imageStore.currentArray.length - 1].data.name;
+            ajaxCall(createUrl(subreddit, "", idAfter, DEFAULT_LIMIT), true);
+        }
+        $("#currentImage").attr("src", imageStore.currentArray[imageStore.imageIndex].data.url);
+    }
+
+    function ajaxCall(formed_url, seekNext) {
+
+        $.ajax({
             type:"GET",
-            url: formed_url,
+            url:formed_url,
             dataType:"json",
-            success: function(data) {
-                picsCallback(data);
+            success:function (data) {
+                picsCallback(data, seekNext);
 
             },
             cache:false
         });
     }
 
-    function picsCallback(data) {
-        $.each(data.data.children, function(i,item){
-            imageArray[i] = data.data.children[i];
-            purifyUrl(imageArray[i]);
+    function picsCallback(data, seekNext) {
+
+        $.each(data.data.children, function (i, item) {
+            imageBuffer[i] = data.data.children[i];
+            purifyUrl(imageBuffer[i]);
         });
 
-        $("<img/>").attr({
-            src: imageArray[0].data.url,
-            id: "currentImage"
-        }).prependTo("#images");
+        if (needsInit === true) {
+            needsInit = false;
+            imageStore.currentArray = imageBuffer.slice(0);
+            $("<img/>").attr({
+                src:imageStore.currentArray[0].data.url,
+                id:"currentImage"
+            }).prependTo("#images");
 
-        id = data.data.children[data.data.children.length-1].data.name;
+            firstId = imageStore.currentArray[0].data.name; // to know when to stop for prev
+
+            var idAfter = imageStore.currentArray[imageStore.currentArray.length - 1].data.name;
+            ajaxCall(createUrl(subreddit, "", idAfter, DEFAULT_LIMIT), true);
+        }
+        else{
+            if (seekNext) // forwards
+                imageStore.nextArray = imageBuffer.slice(0);
+            else // backwards
+                imageStore.prevArray = imageBuffer.slice(0);
+        }
     }
 
-    function purifyUrl(childObject)
-    {
+    function purifyUrl(childObject) {
 
         // imgur
         // qkme.me
 
         var impureUrl = childObject.data.url;
-        console.dir(impureUrl);
         var extension = impureUrl.substr(impureUrl.lastIndexOf('.') + 1);
-        console.dir(extension);
 
-        switch(extension){
+        switch (extension) {
             case 'jpg':
             case 'png':
             case 'gif':
-            break;
+                break;
 
             default:
                 childObject.data.url = DEFAULT_URL;
-                console.log("enterd here!");
-
         }
     }
 
